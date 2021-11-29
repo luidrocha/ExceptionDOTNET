2a67f773c914da7f84984d838090d418a60265a6
Luidrocha <luidrocha@yahoo.com.br>
2021-11-29 09:25:22 -0300

    REserva de Hotel - Solucao Ruim - CSHARP

---
+++ HotelReservaException/Entities/Reserva.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservaException.Entities
+{
+    class Reserva
+        {
+
+            public int Quarto { get; set; }
+            public DateTime Entrada { get; set; }
+            public DateTime Saida { get; set; }
+
+            public Reserva() { }
+
+            public Reserva(int quarto, DateTime entrada, DateTime saida)
+            {
+                Quarto = quarto;
+                Entrada = entrada;
+                Saida = saida;
+            }
+
+            public int Duracao()
+            {
+                TimeSpan duracao = Saida.Subtract(Entrada);
+
+                return (int)duracao.TotalDays; // retorna um double
+
+
+            }
+
+            public void updateReserva(DateTime entrada, DateTime saida)
+            {
+
+                Entrada = entrada;
+                Saida = saida;
+            }
+
+            public override string ToString()
+            {
+
+                return "Quarto: "
+                    + Quarto
+                    + " Entrada: "
+                    + Entrada.ToString("dd/MM/yyyy")
+                    + " Saida: "
+                    + Saida.ToString("dd/MM/yyyy")
+                    + " Duração: "
+                    + Duracao()
+                    + " Noites ";
+            }
+        }
+    }
+}
---
+++ HotelReservaException/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System;
+using ReservaHotelException.Entities;
+namespace ReservaHotelException
+
+namespace HotelReservaException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero do Quarto:  ");
+            int quarto = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o data de Entrada (dd/MM/yyyy) ");
+            DateTime dataE = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Digite o data de Saida (dd/MM/yyyy) ");
+            DateTime dataS = DateTime.Parse(Console.ReadLine());
+
+            if (dataE < DateTime.Now)
+            {
+                Console.WriteLine("Erro na reserva. Entrada não pode ser inferior a data de hoje !");
+
+            }
+            else if (dataE > dataS)
+            {
+                Console.WriteLine("Erro na reserva. Saida não pode ser inferior a data de Entrada !");
+
+            }
+            else
+            {
+
+                Reserva reserva = new Reserva(quarto, dataE, dataS);
+                Console.WriteLine("Reserva: " + reserva);
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Entre com os dados para Atuaizar");
+
+                Console.Write("Digite o data de Entrada (dd/MM/yyyy) ");
+                dataE = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Digite o data de Saida (dd/MM/yyyy) ");
+                dataS = DateTime.Parse(Console.ReadLine());
+
+                DateTime hoje = DateTime.Now;
+
+                if (dataE < hoje || dataS < hoje)
+                {
+                    Console.WriteLine("Erro na reserva: Datas foram do padrão");
+                }
+                else if (dataE > dataS || dataS < dataE)
+                {
+                    Console.WriteLine("Erro na reserva. Saida não pode ser inferior a data de Entrada !");
+
+                }
+                else
+                {
+
+                    reserva.updateReserva(dataE, dataS);
+
+                    Console.WriteLine("Nova reserva: " + reserva);
+                }
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
